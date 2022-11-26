e7f58ca8a0994a7a1398bb95b8cea4d24192e78d
Umang Bhalodiya <umangbhalodiya660@gmail.com>
2022-11-26 23:28:08 +0530

    cmnt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Home from './components/home/Home';
 import './styles/mixins/global.scss';
 
 function App() {
   return (
-    
-      <Home/>
-   );
+    <Home />
+  );
 }
 
 export default App;
